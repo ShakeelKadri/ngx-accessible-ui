7a45b5c873c25cfdc8a44027da5ac49b904e352b
ShakeelKadri <pulsaarcomputer@gmail.com>
2025-03-01 12:02:58 +0530

    update

--- projects/ngx-accessible-ui/src/lib/ngx-accessible-ui.module.ts
+++ projects/ngx-accessible-ui/src/lib/ngx-accessible-ui.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { AccessibleMenuDirective } from './accessible-menu.directive';
 import { AccessibleNavigationDirective } from './accessible-navigation.directive';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [AccessibleMenuDirective, AccessibleNavigationDirective],
-  exports: [AccessibleMenuDirective, AccessibleNavigationDirective],
+  declarations: [
+    AccessibleMenuDirective,
+    AccessibleNavigationDirective
+  ],
+  imports: [CommonModule
+  ],
+  exports: [
+    AccessibleMenuDirective,
+    AccessibleNavigationDirective    
+  ]
 })
-export class NgxAccessibleUiModule {}
+export class NgxAccessibleUiModule { }
+
--- projects/ngx-accessible-ui/src/public-api.ts
+++ projects/ngx-accessible-ui/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of ngx-accessible-ui
  */
 
-export * from './lib/ngx-accessible-ui.module';  // Export module
-export * from './lib/accessible-menu.directive';  // Export directive
-export * from './lib/accessible-navigation.directive';  // Export directive
+export * from './lib/ngx-accessible-ui.module';
+export * from './lib/accessible-menu.directive';
+export * from './lib/accessible-navigation.directive';
+
